1c0d3b3477e7d28140dca013a14a665b11be701a
JoSe85q <johann.seuthe@gmail.com>
2025-03-05 12:54:21 +0100

    Datenbank erstellt, post request zu Item hinzufügen usw

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,30 @@
 const express = require('express');
-
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // TODO: Verbinde ien Datenbank
+
+const db = new sqlite3.Database('./task.db');
 //TODO: Schreibe request/responses
 
+app.use(bodyParser.json());       // Middleware
 
+db.run('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
+
+db.run('INSERT INTO tasks (title) VALUES (?)', "Zähne putzen");
 
 app.get('/', (req, res) => {
-    res.send('request received');
+    res.send('das ding läuft');
+});
+
+app.post('/add', (req, res) => {
+     db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function(){
+     res.json({tag: "Mittwoch", bals_wirds: "Mittag geben"});
+   });
 });
 
 app.listen(3050, "localhost", () => {
-    console.log("bald ist Mittach")
+    console.log("Server ist gestartet")
 })
